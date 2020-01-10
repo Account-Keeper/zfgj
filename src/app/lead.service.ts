44a2c8088f8b476388e0a4ae9b88d0a521e5967b
goauto <goauto@goautos-MacBook-Pro.local>
2020-01-10 14:50:36 -0700

    添加线索服务

---
+++ src/app/lead.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpEventType, HttpParams } from '@angular/common/http';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
+import { catchError, retry, map, first } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
+
+export const url_api = 'http://111.229.24.199:8001/';
+const API_TOKEN = "";
+
+
+@Injectable()
+export class LeadService {
+
+  constructor(
+    private http: HttpClient,
+    public router: Router
+  ) {
+    try{
+    }
+    catch (e) {
+      if (e instanceof SyntaxError) {
+          console.log(e, true);
+      } else {
+          console.log(e, false);
+      }
+  }
+
+   }
+
+  private getHeaders(headersConfig?: object): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With,content-type, X-Token-Auth, Authorization',
+      'Access-Control-Allow-Credentials': 'true'
+    });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
+  getLeads(filter) {
+    const params = new HttpParams()
+        .set("api_token", API_TOKEN);
+
+    return this.http.get<any>(url_api+'leads', {headers: this.getHeaders()})
+    .pipe(map(data=>{
+      if(data){
+        return data;
+      }
+      return data;
+    }));
+  }
+
+}
+
+
+//external functions
+
--- src/app/leads/leads.component.ts
+++ src/app/leads/leads.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LeadService } from '../lead.service';
 
 @Component({
   selector: 'app-leads',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leads.component.css']
 })
 export class LeadsComponent implements OnInit {
+  private leads = [];
 
-  constructor() { }
+  constructor(
+    private lead_service: LeadService
+  ) { }
 
   ngOnInit() {
+  }
+
+  getLeads() {
+    this.lead_service.getLeads({}).subscribe(data=>{
+      if(data){
+        let arr = [...data['results']];
+        arr.forEach(item=>{
+        });
+        this.leads = arr;
+      }
+    });
   }
 
 }
